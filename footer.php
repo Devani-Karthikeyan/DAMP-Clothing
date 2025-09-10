90961cdee68a93513c7b236e62e963b661510b38
Devani-Karthikeyan <devaniselva31@gmail.com>
2025-09-10 15:32:57 +0530

    Upload Footer

---
+++ footer.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href="footer.css">
+    
+</head>
+<body>
+    <div class="footer">
+    <div class="row">
+      <!-- Column 1 -->
+      <div class="footer-col">
+        
+        <ul>
+            <li><img src="new-logo.png" alt="logo"></li>
+            <li><a href="">Phone:+94710892203</a></li>
+            <li><a href="">Email:dampstyles@gmail.com</a></li>
+        </ul>
+    </div>
+
+        <!-- Column 2 -->
+        <div class="footer-col">
+            <h3>OUR STORES</h3>
+            <ul>
+            <li>DAPM Styles Kelaniya</li>
+            <li>DAPM Styles Wellawatte</li>
+            <li>DAPM Styles Jaffna</li>
+            <li>DAPM Styles Galle</li>
+            
+            </ul>
+        </div>
+
+      <!-- Column 3 -->
+      <div class="footer-col">
+        <h3>Customer Service</h3>
+        <ul>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Return Policy</a></li>
+          <li><a href="#">Shipping</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+        </ul>
+      </div>
+
+      <!-- Column 4 -->
+      <div class="footer-col">
+        <h3>Follow Us</h3>
+        <div class="social-links">
+          <a href="#"><i class="fa-brands fa-facebook"></i></a>
+          <a href="#"><i class="fa-brands fa-instagram"></i></a>
+          <a href="#"><i class="fa-brands fa-twitter"></i></a>
+          <a href="#"><i class="fa-brands fa-youtube"></i></a>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+    <p class="copyright">© 2025 DAMP Styles. All Rights Reserved.</p>
+  
+</div>
+    
+</body>
+</html>
